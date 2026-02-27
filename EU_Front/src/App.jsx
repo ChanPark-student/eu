7cb97957a953a828b8cdc65acecaf7f7fd5fe4ce
leeseungyeol03 <ppqq8008@gmail.com>
2026-02-28 01:36:46 +0900

    Use HashRouter to prevent refresh 404 on Render static hosting

--- EU_Front/src/App.jsx
+++ EU_Front/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import Home from './pages/Home';
 import Login from './pages/Login';
